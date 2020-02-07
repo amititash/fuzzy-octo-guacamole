e0f4bc803d2b3eff974ab212ff01d8b148b9728c
Yash Pratap Singh <singhyashpratap06@gmail.com>
2020-02-07 15:06:11 +0530

    minor fix

--- server/api/middlewares/ko/ko.js
+++ server/api/middlewares/ko/ko.js
@@ -7,26 +7,33 @@ const uniqueIdeaNameModifier = async (req, res, next) => {
     else {
         console.log("yyyyyyyyyyyyyyy");
         let count = 0;
-        let ideaName = req.body.ideaName || 'my-idea' ;
         let ideaOwner = req.body.ideaOwner ;
-        let criteria = {
-            "ideaName" : {
-            "$regex" : ideaName
-            },
+        let criteria = {};
+
+        let ideaName = req.body.ideaName ;
+        criteria = {
+            // "ideaName" : {
+            // "$regex" : ideaName
+            // },
             "ideaOwner" : ideaOwner
         }
         try {
             count = await KoService.countKo(criteria);
-            if(count !== 0){
-                ideaName = ideaName + `-${count}`
-            }
-            req.body.ideaName = ideaName;
-            console.log("idea name to be saved", ideaName);
         }
         catch(e){
             console.log(e);
             next(e);
         }
+        if(ideaName === "my-idea"){
+            ideaName += `-${count}`
+        }
+        else {
+            if(count){
+                ideaName += `-${count}`
+            }
+        }
+        req.body.ideaName = ideaName;
+        console.log("idea name to be saved", ideaName);
         next()
     }
     
