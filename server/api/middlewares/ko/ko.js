bab5b0c357d2f717cc4c5b4ed222c117afc5a899
Yash Pratap Singh <singhyashpratap06@gmail.com>
2020-02-07 14:22:15 +0530

    bug fix-> idea name being updated in other deepdive sections as well leading to error

--- server/api/middlewares/ko/ko.js
+++ server/api/middlewares/ko/ko.js
@@ -1,28 +1,35 @@
 import  KoService from '../../services/kos.service';
 
 const uniqueIdeaNameModifier = async (req, res, next) => {
-    let count = 0;
-    let ideaName = req.body.ideaName || 'my-idea' ;
-    let ideaOwner = req.body.ideaOwner ;
-    let criteria = {
-        "ideaName" : {
-          "$regex" : ideaName
-        },
-        "ideaOwner" : ideaOwner
+    if(!req.body.ideaName){
+        next();
     }
-    try {
-        count = await KoService.countKo(criteria);
-        if(count !== 0){
-            ideaName = ideaName + `-${count}`
+    else {
+        console.log("yyyyyyyyyyyyyyy");
+        let count = 0;
+        let ideaName = req.body.ideaName || 'my-idea' ;
+        let ideaOwner = req.body.ideaOwner ;
+        let criteria = {
+            "ideaName" : {
+            "$regex" : ideaName
+            },
+            "ideaOwner" : ideaOwner
         }
-        req.body.ideaName = ideaName;
-        console.log("idea name to be saved", ideaName);
+        try {
+            count = await KoService.countKo(criteria);
+            if(count !== 0){
+                ideaName = ideaName + `-${count}`
+            }
+            req.body.ideaName = ideaName;
+            console.log("idea name to be saved", ideaName);
+        }
+        catch(e){
+            console.log(e);
+            next(e);
+        }
+        next()
     }
-    catch(e){
-        console.log(e);
-        next(e);
-    }
-    next()
+    
 }
 
 
