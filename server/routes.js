ea0aca75ca6613469314d04c93b25361debb2dca
Yash Pratap Singh <singhyashpratap06@gmail.com>
2020-03-24 16:48:30 +0530

    add route for admin page

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 import examplesRouter from './api/controllers/examples/router';
 import usersRouter from './api/controllers/users/router';
 import storiesRouter from './api/controllers/kos/router';
+import adminRouter from './api/controllers/admin/router';
 
 export default function routes(app) {
   app.get('/ping', (req, res) => {
@@ -11,4 +12,5 @@ export default function routes(app) {
   app.use('/api/v1/examples' , examplesRouter);
   app.use('/api/v1/users', usersRouter);
   app.use('/api/v1/kos', storiesRouter);
+  app.use('/api/v1/admin' , adminRouter)
 }
